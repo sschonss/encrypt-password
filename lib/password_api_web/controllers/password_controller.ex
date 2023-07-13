584ca3cbda85effec90262c19163c5c5b1e0bf54
sschonss <schonsluuiz@gmail.com>
2023-07-13 20:57:22 -0300

    Remove Comments

--- lib/password_api_web/controllers/password_controller.ex
+++ lib/password_api_web/controllers/password_controller.ex
@@ -2,7 +2,6 @@ defmodule PasswordApiWeb.PasswordController do
   use PasswordApiWeb, :controller
 
   def encrypt_password(conn, %{"password" => password}) do
-    #make cryptographically secure salt using bcrypt_elixir
     encrypted_password = Bcrypt.hash_pwd_salt(password)
     json(conn, %{encrypted_password: encrypted_password})
   end
